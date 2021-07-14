e3b84f91fc22e1bfc84ab08a80cbef321e6f75f8
Honza Brestan <jbrestan@users.noreply.github.com>
2021-07-14 20:07:54 +0200

    Add default stricturizr workspace

---
+++ run-structurizr.sh
@@ -0,0 +1,4 @@
+DOCS_PATH=$( cd "$(dirname "$0")" ; pwd -P )
+echo "Starting structurizr/lite in $DOCS_PATH"
+docker pull structurizr/lite
+docker run -it --rm -p 8080:8080 -v $DOCS_PATH:/usr/local/structurizr structurizr/lite
