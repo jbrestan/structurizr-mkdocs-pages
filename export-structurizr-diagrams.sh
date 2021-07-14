ee35631ff3bd14a5060e866a7901e8b7981b51fb
Honza Brestan <jbrestan@users.noreply.github.com>
2021-07-14 21:20:39 +0200

    Fix diagram generation and add site url

--- export-structurizr-diagrams.sh
+++ export-structurizr-diagrams.sh
@@ -3,4 +3,4 @@
 DOCS_PATH=$( cd "$(dirname "$0")" ; pwd -P )
 docker pull ghcr.io/aidmax/structurizr-cli-docker
 docker run --rm -v $DOCS_PATH:/root/data -w /root/data ghcr.io/aidmax/structurizr-cli-docker \
-    export --workspace workspace.dsl --format mermaid --output $DOCS_PATH/diagrams/
+    export --workspace workspace.dsl --format mermaid --output docs/diagrams/
